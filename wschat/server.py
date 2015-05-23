04c8302bec4857fba9af3987c5f92e6a9a2e837c
Pavel Bass <statgg@gmail.com>
2015-05-24 01:53:51 +0400

    Added authorization by WebSocket connection. Instantiated login, logout, register commands

--- wschat/server.py
+++ wschat/server.py
@@ -94,12 +94,19 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
     def __init__(self, *args, **kwargs):
         super(ChatHandler, self).__init__(*args, **kwargs)
         self.db = DB
+        self._user = None
+        self.knows_commands = dict(
+            login=self.command_login,
+            logout=self.command_logout,
+            register=self.command_register
+        )
 
     def open(self):
-        user = self.current_user
+        user = self.get_secure_cookie('user')
         if user is None:
             self.connect_to_room(self.db.default_room)
         else:
+            self._user = user
             for room in self.db.get_current_rooms(user):
                 self.connect_to_room(room)
 
@@ -113,10 +120,19 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
         user = self.current_user
         if user is None:
             user = 'Anonymous'
-        mess = '%s: %s' % (user, tornado.escape.xhtml_escape(mess))
-        for room in rooms:
-            self.db.new_message(room, mess)
-            self.send_to_waiters(room, mess)
+        if mess.startswith('#'):
+            mess = mess[1:].split(' ', 1)
+            command = mess[0]
+            args = mess[1] if len(mess) - 1 else ''
+            if command not in self.knows_commands:
+                self.send_server_message('Unknown command')
+            else:
+                self.knows_commands[command.lower()](args)
+        else:
+            mess = '%s: %s' % (user, tornado.escape.xhtml_escape(mess))
+            for room in rooms:
+                self.db.new_message(room, mess)
+                self.send_to_waiters(room, mess)
 
     def send_to_waiters(self, room, mess):
         mess = 'MESSAGE:[%s] %s' % (room, mess)
@@ -151,15 +167,56 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
             mess = 'MESSAGE:[%s] %s' % (room, mess)
             self.write_message(mess)
 
+    @property
+    def current_user(self):
+        return self.get_current_user()
+
     def get_current_user(self):
-        return self.get_secure_cookie('user')
+        return self._user
 
     @property
     def current_rooms(self):
         user = self.current_user
         return self.db.get_current_rooms(user)
 
+    def command_login(self, login_password):
+        login_password = filter(lambda x: x, login_password.strip(' ').split(' '))
+        mess = ''
+        try:
+            login, password = login_password[0], login_password[1]
+            user = self.db.is_correct_user(login, password)
+        except IndexError:
+            mess = 'Wrong command usage'
+        if mess:
+            pass
+        elif user is None:
+            mess = 'No such user'
+        elif not user:
+            mess = 'Password incorrect'
+        else:
+            mess = 'You are logged in as "%s"' % login
+            self._user = login
+        self.send_server_message(mess)
 
+    def command_logout(self, args):
+        self._user = None
+        self.send_server_message('You are logged out')
+
+    def command_register(self, login_password):
+        login_password = filter(lambda x: x, login_password.strip(' ').split(' '))
+        mess = ''
+        try:
+            login, password = login_password[0], login_password[1]
+        except IndexError:
+            mess = 'Wrong command usage'
+        if not mess:
+            user = self.db.new_user(login, password)
+            if user is None:
+                mess = 'Such user already exists'
+            else:
+                mess = 'User "%s" successfully created. Try to login.' % login
+        self.send_server_message(mess)
+        
 def main(host, port):
     handlers = [
         (r"/", MainHandler),
