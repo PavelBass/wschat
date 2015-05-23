cd26f586c07e7664037f063adcba093a407835e7
Pavel Bass <statgg@gmail.com>
2015-05-23 21:52:18 +0400

    Removed message/command user detecting

--- wschat/static/client.js
+++ wschat/static/client.js
@@ -6,26 +6,18 @@ function check_ws(){
     }
 }
 
-ws = new WebSocket(socket_path);
+var ws = new WebSocket(socket_path);
 elem_to_write = document.getElementById('chat');
 
-function send_message(mess){
-    mess = 'MESSAGE:' + mess;
-    ws.send(mess)
-}
-
-function send_command(mess){
-    mess = 'COMMAND:' + mess;
-}
-
 ws.onmessage = function(evnt){
+    console.log(evnt.data)
     var mess = evnt.data;
     var num = mess.indexOf(':');
     var command = mess.substr(0, num);
     mess = mess.substr(num+1);
-    if (command=='MESSAGE:'){
+    if (command=='MESSAGE'){
         write_message(mess);
-    }else if (command=='SERVER:'){
+    }else if (command=='SERVER'){
         write_server_message(mess);
     }
 };
@@ -37,16 +29,14 @@ function write_message(mess){
 
 function write_server_message(mess){
     var source = elem_to_write.innerHTML;
-    elem_to_write.innerHTML = source + '<error>'+mess+'</error>';
+    elem_to_write.innerHTML = source + '<server>'+mess+'</server>';
 }
 
-function sender(){
-    var elem = document.getElementById('sender');
-    var mess = elem.val();
-    mess = mess.trim();
-    if (mess[0] == '#'){
-        send_command(mess);
-    }else{
-        send_message(mess);
-    }
+function sender_click(){
+    var elem = document.getElementById('sendmessage');
+    var mess = elem.value;
+    ws.send(mess);
+    elem.value = '';
 }
+
+document.getElementById('sender_button').onclick = sender_click;
