74d0cf4a714a58171c9a8daed299d51250117f65
Pavel Bass <statgg@gmail.com>
2015-05-24 04:40:07 +0400

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "wschat",
-    version = "0.1",
+    version = "0.6",
     packages = find_packages(exclude=["tests"]),
 
     author = "Pavel V. Bass",
